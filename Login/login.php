b2c51dd62708fa6736cfa4b0118face7b11ce580
4m3l14 <113897169+4m3l14@users.noreply.github.com>
2023-10-06 09:41:16 +0200

    Update login.php

--- Login/login.php
+++ Login/login.php
@@ -1,20 +1,41 @@
 <html>
 <head>
-<title>Form</title>
+    <title>Form</title>
 </head>
 <body>
-<h1>Login</h1>
-<?php
-$usr=$_POST["username"];
-$pwd=$_POST["password"];
-if($usr!="Flavio" || $pwd!="ciao") {
-?>
-<h1>Errore! Username o password errati.</br>
-Accesso negato!</h1>
-<?php
-} else{
-  echo "<h1>Benvenuto ". $usr. "</h1>";
-}
-?>
+    <h1>Login</h1>
+
+    <?php
+    $usr=$_POST["username"];
+    $pwd=$_POST["password"];
+    if($usr!="Amelia" || $pwd!="ciao") {
+    ?>
+        <h1>Errore! Username o password errati.<br/>
+        Accesso negato!</h1>
+    <?php
+    } else {
+        echo "<h1>Benvenuto $usr nella pagina riservata del sito.</h1>";
+    }
+    ?>
 </body>
 </html>
+
+
+*\<html>
+<head>
+    <title>Form</title>
+</head>
+<body>
+    <h1>Login</h1>
+
+    <?php
+    $usr=$_POST["username"];
+    $pwd=$_POST["password"];
+    if($usr!="Amelia" || $pwd!="ciao") {
+        echo "W<h1>Errore! Username o password errati.<br/>Accesso negato!</h1>";
+    } else {
+        echo "<h1>Benvenuto $usr nella pagina riservata del sito.</h1>";
+    }
+    ?>
+</body>
+</html>*\
