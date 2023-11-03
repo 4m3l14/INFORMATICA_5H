42f4fe41b7a241c302558a2f208ba5a01c5948f3
4m3l14 <113897169+4m3l14@users.noreply.github.com>
2023-11-03 09:40:22 +0100

    Update logout.php

--- ES04/ES04A/logout.php
+++ ES04/ES04A/logout.php
@@ -1,1 +1,14 @@
+<html>
+    <head>
+        <title>LOGOUT</title>
+    </head>
+    <body>
+        <?php
+        session_start();
+        session_destroy();
+        header("Location: index.php");
+        exit;
+        ?>
+    </body>
+</html>
 
