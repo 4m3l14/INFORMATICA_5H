753e759bc2b3dc761850c995f0ee725dd716491c
4m3l14 <113897169+4m3l14@users.noreply.github.com>
2023-11-10 10:02:19 +0100

    Update riservata1.php

--- ES04/ES04A/riservata1.php
+++ ES04/ES04A/riservata1.php
@@ -2,7 +2,12 @@
 session_start();
 
 if (!isset($_SESSION["username"])) {
-    header("Location: login.php?error=Attenzione! Prima di accedere alla sezione riservata dle sito e' necessario effetuare il login.");
+    $error_message = "Attenzione! Prima di accedere alla sezione riservata dle sito e' necessario effetuare il login.";
+    header("Location: login.php?error=".urlencode($err_message));
+    exit;
+}
+if (isset($_GET['logout']) && $_GET['logout'] === 'true') {
+    header("Location: logout.php");
     exit;
 }
 ?>
@@ -12,20 +17,8 @@ if (!isset($_SESSION["username"])) {
     <title>Riservata</title>
 </head>
 <body>
-    <?php
-    if (isset($_SESSION["username"])) {
-    ?>
-        <p><h1>Benvenuto <?php echo $_SESSION["username"]; ?></h1></p>
-        <a href="index.php">Vai alla home del sito</a><br>
-        <a href="logout.php">Logout</a>
-    <?php   
-    } else {
-    ?>
-        <font color="red"><p><h2>Non sei autenticato!</h2></p></font><br>
-        <a href="index.php">Vai alla home del sito</a><br>
-        <a href="login.php">Login</a>
-    <?php
-    }
-    ?>
+<h2>Ciao, mrossi. Benvenuto nell'area riservata del sito.</h2>
+    <p><a href="logout.php">Logout</a></p>
+</body>
 </body>
 </html>
