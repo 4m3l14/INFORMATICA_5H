3d27be4574967630b9ffb6d15915d08f07c80654
4m3l14 <113897169+4m3l14@users.noreply.github.com>
2023-11-10 10:02:03 +0100

    Update login.php

--- ES04/ES04A/login.php
+++ ES04/ES04A/login.php
@@ -31,3 +31,52 @@ END;
     ?>
 </body>
 </html>
+
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header('Location: login.php');
+    exit();
+}
+$valid_username = 'mrossi';
+$valid_password = 'Ciao123';
+$err_message = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if ($username === $valid_username && $password === $valid_password) {
+        $_SESSION['username'] = true;
+        header('Location: riservata.php');
+        exit();
+    } else {
+        $err_message = 'Attenzione! Le credenziali inserite non sono corrette.';
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LOGIN</title>
+</head>
+<body>
+    <h1>Vai all'area riservata del sito.</h1>
+    <form method="POST">
+        <label for="username">Nome utente:</label>
+        <input type="text" name="username" id="username" required><br><br>
+        
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" required><br><br>
+        <input type="submit" value="Accedi">
+    </form>
+    <?php
+
+    if (!empty($err_message)) {
+        echo "<p>$err_message</p>";
+    }
+    ?>
+</body>
+</html>
