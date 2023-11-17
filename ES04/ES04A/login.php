e0f9874eb34ceb7cb9cbd45a14f2958cef69aa98
4m3l14 <113897169+4m3l14@users.noreply.github.com>
2023-11-17 10:04:42 +0100

    Update login.php

--- ES04/ES04A/login.php
+++ ES04/ES04A/login.php
@@ -1,45 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>LOGIN</title>
-</head>
-<body>
-    <?php
-    session_start();
-    echo <<<END
-    <form name="formLogin" method="post" action="{$_SERVER["PHP_SELF"]}">
-        Username: <input type="text" name="username"> <br>
-        Password: <input type="password" name="password"></br>
-        <input type="submit" name="invio" value="invio"><br>
-        <input type="reset" value="cancella"> </br>
-    </form>
-END;
-
-    $user = $_POST["username"];
-    $pass = $_POST["password"];
-    controlloUser($user);
-    controlloPass($pass);
-    if ($user == "mrossi" && $pass == "Ciao123") {
-        $_SESSION["username"] = $user;
-        $_SESSION["password"] = $pass;
-        header("Location: riservata.php");
-        exit;
-    } else {
-        echo "Errore! Username o password errati.";
-        exit;
-    }
-    ?>
-</body>
-</html>
-
 <?php
 session_start();
-if (!isset($_SESSION['username'])) {
-    header('Location: login.php');
+if (isset($_SESSION['username'])) {
+    header('Location: riservata.php');
     exit();
 }
-$valid_username = 'mrossi';
-$valid_password = 'Ciao123';
+$valido_s = 'mrossi';
+$valido_p = 'Ciao123';
 $err_message = '';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -47,8 +13,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    if ($username === $valid_username && $password === $valid_password) {
-        $_SESSION['username'] = true;
+    if ($username === $valido_s && $password === $valido_p) {
+        $_SESSION['username'] = $username;
         header('Location: riservata.php');
         exit();
     } else {
